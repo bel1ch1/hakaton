7723e7acfadd0c7603ee8d41c1593c41f55b4679
bel1c1 <andreyzv5555@gmail.com>
2024-04-05 19:43:13 +0300

    get universe

--- access.py
+++ access.py
@@ -1,19 +1,23 @@
 import requests
-from config import PATH_TO_SERVER, TOKEN
+from config import PATH_TO_SERVER, TOKEN, GET_UNIVERSE
 
 
 headers = {
     "X-Auth-Token" : f"{TOKEN}"
 }
 
+
 def get_universe():
-    data = requests.get(PATH_TO_SERVER+"/player/universe", headers=headers)
+    data = requests.get(PATH_TO_SERVER+GET_UNIVERSE, headers=headers)
     print(data.text)
 
 
 def make_move():
-    pass
+    requests.post(PATH_TO_SERVER)
 
 
 def tetris_logic():
     pass
+
+
+get_universe()
