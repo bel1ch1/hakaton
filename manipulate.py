d0a243e81cd4e52fc06c4c07efaf7be003086891
maximalekseenko <max.alekseenko@gmail.com>
2024-04-05 19:34:10 +0300

    manipulate.py docstring and human-readable code fix

--- manipulate.py
+++ manipulate.py
@@ -1,8 +1,12 @@
-"""Functions for fucking with garbage and planets"""
+"""Functions for fucking with garbage and planets."""
 
 
 
 def RotateGarbage(__garb):
+    """Rotates garbage clockwise once.
+    
+    * `__garb`: garbage to rotate
+    """
 
     # find size of this garbage
     garb_width = 0
@@ -11,8 +15,8 @@ def RotateGarbage(__garb):
         if _tile[0] > garb_width:  garb_width = _tile[0]
         if _tile[1] > garb_height: garb_height = _tile[1]
 
-    # rotate and return
-    return [
+    # rotate
+    manipulated_garb = [
         [
             _tile[1],
             garb_width-_tile[0]
@@ -20,14 +24,27 @@ def RotateGarbage(__garb):
         for _tile in __garb
     ]
 
+    # return
+    return manipulated_garb
 
-def MoveGarbage(__garb, __x, __y):
 
-    # moave and return
-    return [
+
+def MoveGarbage(__garb:list[list[int]], __x:int, __y:int):
+    """Moves garbage in direction.
+    
+    * `__garb`: garbage to move
+    * `__x`: distance to move in x axis
+    * `__y`: distance to move in y axis
+    """
+
+    # move
+    manipulated_garb = [
         [
             _tile[0] + __x,
             _tile[1] + __y
         ]
         for _tile in __garb
     ]
+
+    # return
+    return manipulated_garb
