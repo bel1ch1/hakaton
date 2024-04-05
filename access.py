20e5c5ebc41cb8b43f19ea59ff288854f093cd34
bel1c1 <andreyzv5555@gmail.com>
2024-04-05 19:10:21 +0300

    access to server

--- access.py
+++ access.py
@@ -3,7 +3,7 @@ from config import PATH_TO_SERVER, TOKEN
 
 
 headers = {
-    ApiKeyAuth
+    "X-Auth-Token" : f"{TOKEN}"
 }
 
 data = requests.get(PATH_TO_SERVER+"/player/universe", headers=headers)
