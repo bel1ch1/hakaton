2d8ebbca75bba1239171b9737f44422b496568cd
bel1c1 <andreyzv5555@gmail.com>
2024-04-05 21:09:33 +0300

    generate path

---
+++ generate_path.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+# Парсим json
+def json_serializer():
+    pass
+
+
+# Получаем текушию инфу о планетах
+def currency_universe() -> List:
+    pass
+
+
+# Генерим путь по которому полетим
+def generate_path() -> List:
+    pass
