12a87ad21026680e36705c7d66e8936490b296bb
bel1c1 <andreyzv5555@gmail.com>
2024-04-06 11:44:47 +0300

    dsa

--- access.py
+++ access.py
@@ -1,5 +1,7 @@
 import requests
 from config import PATH_TO_SERVER, TOKEN, GET_UNIVERSE, TRAVEL, COLLECT
+from generate_path import Generate_path
+import json
 
 
 headers = {
@@ -16,13 +18,6 @@ headers3 = {
     "Content-Type" : "application/json",
 }
 
-
-movet_movet = {
-  "planets": [
-    "V2VPTYB55",
-    "Gaylord"
-  ]
-}
 
 
 collect_garb = {
@@ -48,13 +43,14 @@ collect_garb = {
 }
 }
 
+
 # <--------------------- Главные функции запросов -------------------------->
 def get_universe():
     data = requests.get(PATH_TO_SERVER+GET_UNIVERSE, headers=headers, json=movet_movet)
-    print(data.text)
+    return data
 
 
-def make_move():
+def make_move(movet_movet):
     move_data = requests.post(PATH_TO_SERVER+TRAVEL, headers=headers2, json=movet_movet)
     print(move_data.text)
 
@@ -64,6 +60,40 @@ def tetris_logic():
     print(collect.text)
 
 
-#get_universe()
-make_move()
-#tetris_logic()
+def generate_movet_movet(current_planet, next_planet):
+    movet_movet = {
+        "planets": [
+        f"{current_planet}",
+        f"{next_planet}"
+        ]
+    }
+    return movet_movet
+
+
+# make_move()
+# tetris_logic()
+
+next_planets_way = []
+interest_points = []
+stok = "Eden"
+
+
+def for_data_py():
+    universe = get_universe()
+    if universe.status_code == 200:
+
+        data = universe.json()
+        planets = data.get("universe")
+        start_pose = data["universe"][0][0]
+
+        for i in range(len(planets)-1):
+            if planets[i][1] == start_pose:
+                last_planet = start_pose
+                next_planet = planets[i][1]
+                movet = generate_movet_movet(last_planet, next_planet)
+                make_move(movet)
+
+
+
+            #next_planets_way.append(planets[i])
+    #print(next_planets_way)
--- generate_path.py
+++ generate_path.py
@@ -1,17 +1,12 @@
 from typing import Dict
-import json
+
 
 
 class Generate_path:
-    def __init__(self, current_universe):
-        self.universe = current_universe
-
-    # Получаем текущую инфу о планетах
-    def currency_universe(universe) -> Dict:
-        uverse = json.loads(universe)
-        return uverse
-
+    def __init__(self, planets) -> None:
+         self.planets = planets
 
     # Генерим путь по которому полетим
-    def generate_path(dict_data):
+    def generate_path(start_pose):
         pass
+        #from_Earth =
