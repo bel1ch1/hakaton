915b27928f743ba89d9a9c17b5d2284bd740bcfc
maximalekseenko <max.alekseenko@gmail.com>
2024-04-06 11:22:58 +0300

    added data.py

---
+++ data.py
@@ -0,0 +1,18 @@
+"""Current data for access"""
+
+DATA = {
+    "name": "",
+    "roundName": "",
+    "roundEndIn": 0,
+    "ship": {
+        "fuelUsed": 0,
+        "planet": {
+            "name": "",
+            "garbage": {}
+        },
+        "capacityX": 8,
+        "capacityY": 11,
+        "garbage": {}
+    },
+    "universe": []
+}
